45c38ebea9231bc70200a3b4874c653c1b9ee24f
ledlaputa <ledlaputa@gmail.com>
2020-03-16 01:37:53 +0900

    Initial commit

---
+++ app/src/main/java/com/ledlaputa/cinema/MainActivity.java
@@ -0,0 +1,17 @@
+package com.ledlaputa.cinema;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ScrollView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
